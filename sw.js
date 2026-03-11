bdb5638ba10713f4f33c3b7c27d9c368025f5c21
andersonfonsec4 <136765684+andersonfonsec4@users.noreply.github.com>
2026-03-11 08:00:56 -0300

    blue-player-v02
    Acrescentei os icones

--- sw.js
+++ sw.js
@@ -1,25 +1,36 @@
-const CACHE_NAME = "blueplayer-v1";
+const CACHE_NAME = "blueplayer-cache-v1";
 
 const FILES = [
   "./",
   "./index.html",
 
+  "./icon-192.png",
+  "./icon-512.png",
+
+  "./manifest.json",
+
   "./src/css/style.css",
 
   "./src/js/player.js",
-  "./src/js/ui.js",
   "./src/js/playlist.js",
+  "./src/js/ui.js",
   "./src/js/visualizer.js",
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES)));
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open(CACHE_NAME)
+
+      .then((cache) => cache.addAll(FILES)),
+  );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
     caches
-      .match(event.request)
-      .then((response) => response || fetch(event.request)),
+      .match(e.request)
+
+      .then((response) => response || fetch(e.request)),
   );
 });
